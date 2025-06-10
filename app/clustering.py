fbbe9d9bc4337de52a595399fdd704097ef165c9
bandirishi23 <144281031+bandirishi23@users.noreply.github.com>
2025-06-10 14:14:28 +0530

    Update clustering.py

--- app/clustering.py
+++ app/clustering.py
@@ -1,6 +1,8 @@
 from sklearn.metrics.pairwise import cosine_similarity
 import uuid
 import logging
+import os
+COSINE_SIMILARITY_THRESHOLD = float(os.getenv("SIMILARITY_THRESHOLD", "0.8"))
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -17,7 +19,7 @@ def find_or_create_theme(thesis_vec, existing_themes):
 
     logger.info(f"Highest similarity: {highest_similarity:.4f} for theme_id: {closest_theme_id}")
 
-    if highest_similarity >= 0.8:
+    if highest_similarity >= COSINE_SIMILARITY_THRESHOLD:
         return closest_theme_id
     else:
         return str(uuid.uuid4())
